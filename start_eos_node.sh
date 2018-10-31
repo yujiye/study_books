60b8957a49dfb9aabe587505c4a7b9338eacbcf4
root <root@localhost.localdomain>
2018-10-31 21:39:44 +0800

    centos启动EOS NODE 的时候，映射目录失败，需要注意和ubuntu的区别 centos的SELINUX有安全配置，所以要
    --priviliged=true
    
    详情看start_eos_node.sh

---
+++ start_eos_node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "start nodeos !"
+
+/usr/bin/docker run --rm --name nodeos -v /home/eosio/data-dir:/opt/eosio/bin/data-dir --privileged=true -p 8888:8888 -p 9876:9876 -t eosio/eos nodeosd.sh --max-transaction-time=1000 -e --http-alias=nodeos:8888 --http-alias=127.0.0.1:8888 --http-alias=localhost:8888 arg1 arg2
